944d3edf6a10d520ab96eb91d731f92233156471
Bryan Duggan <skooter500@gmail.com>
2021-02-04 09:56:25 +0000

    Bug landing!

--- java/src/ie/tudublin/BugZap.java
+++ java/src/ie/tudublin/BugZap.java
@@ -98,7 +98,7 @@ public class BugZap extends PApplet {
 	}
 
 	void moveBug() {
-		if ((frameCount % 30) == 0) {
+		if ((frameCount % 1) == 0) {
 			bugX += random(-5, 5);
 			if (bugX < halfBugWidth) {
 				bugX = halfBugWidth;
@@ -110,13 +110,29 @@ public class BugZap extends PApplet {
 		}
 	}
 
+	int gameMode = 0;
+
 	public void draw() {
 		background(0);
-		fill(255);
-		drawPlayer(playerX, playerY, playerWidth);
-		drawBug(bugX, bugY);
-		moveBug();
+		if (gameMode == 0)
+		{
+			fill(255);
+			drawPlayer(playerX, playerY, playerWidth);
+			drawBug(bugX, bugY);
+			moveBug();
 
-		text("Score: " + score, 20, 20);
+			text("Score: " + score, 20, 20);
+		}
+		else
+		{
+			textAlign(CENTER, CENTER);
+			text("GAME OVER!!!", width / 2, height / 2);
+		}
+
+		if (bugY > height - 50)
+		{
+			gameMode = 1;
+		}
+
 	}
 }
