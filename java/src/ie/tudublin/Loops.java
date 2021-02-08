336c12c828e90e353eb207d09cf381781277bd53
Bryan Duggan <skooter500@gmail.com>
2021-02-08 14:26:37 +0000

    exercises

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -28,11 +28,63 @@ public class Loops extends PApplet {
 
     public void draw() {
         background(0);
+        noStroke();
         switch (mode)
         {
             case 0:
-                ellipse(cx, cy, 100, 100);
+                fill(50, 255, 255);                    
+                if (mouseX < cx)
+                {
+                    rect(0, 0, cx, height);
+                }
+                else
+                {
+                    rect(cx, 0, cx, height);
+                }
                 break;
+            case 1:
+                fill(50, 255, 255);                                    
+                if (mouseX < cx && mouseY < cy)
+                {
+                    rect(0, 0, cx, cy);
+                }
+                else if (mouseX > cx && mouseY < cy)
+                {
+                    rect(cx, 0, cx, cy);
+                }
+                else if (mouseX < cx && mouseY > cy)
+                {
+                    rect(0, cy, cx, cy);
+                }
+                else
+                {
+                    rect(cx, cy, cx, cy);
+                }
+                break;
+            case 2:
+            {
+                int numRects = (int)(mouseX / 10.0f);
+                float w = width / (float) numRects;
+                float cgap = 255 / (float) numRects;
+                for(int i = 0 ; i < numRects ; i ++)
+                {
+                    fill(i * cgap, 255, 255);
+                    rect(i * w, 0, w, height);
+                }
+                break;
+            }
+            case 3:
+            {
+                int numCircles = (int)(mouseX / 10.0f);
+                float w = width / (float) numCircles;
+                float cgap = 255 / (float) numCircles;
+                for(int i = 0 ; i < numCircles ; i ++)
+                {
+                    fill(cgap * i, 255, 255);
+                    ellipse(w / 2 + (i * w), cy, w, w);
+                }
+            }
+            break;
         }
     }
 }
