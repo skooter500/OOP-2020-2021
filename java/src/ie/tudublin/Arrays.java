e6b87025c25a856357161b08371008e6a0658951
Bryan Duggan <skooter500@gmail.com>
2021-02-22 12:00:52 +0000

    bla

--- java/src/ie/tudublin/Arrays.java
+++ java/src/ie/tudublin/Arrays.java
@@ -38,7 +38,8 @@ public class Arrays extends PApplet {
     }
 
     public void settings() {
-        size(500, 500);
+        size(1000, 1000);
+
 
         // Testing the map function
         float f = map1(2, 0, 10, 0, width);
@@ -125,11 +126,64 @@ public class Arrays extends PApplet {
         background(0);
         switch (mode) {
             case 0: {
-                // Bar chart
+                float border = width * 0.1f;
+                stroke(255);
+                colorMode(HSB);
+                line(border, border, border, height - border);
+                line(border, height - border, width - border, height - border);
+                textAlign(CENTER, CENTER);
+                for(float f = 0 ; f <= 120 ; f += 10)
+                {
+                    float y = map(f, 0, 120, height - border, border);
+                    line(border - 5, y, border, y);
+                    fill(255);
+                    text((int) f, border * 0.5f, y);
+                }
+
+                float w = (width - border * 2) / (float) rainfall.length;
+                for(int i = 0 ; i < rainfall.length ; i ++)
+                {
+                    float x = map(i, 0, rainfall.length, border, width - border);
+                    float c = map(i, 0, rainfall.length, 0, 255);
+                    fill(c, 255, 255);
+                    float h = map(rainfall[i], 0, 120, 0, -(height - (border * 2)));
+                    rect(x, height - border - 1, w, h);
+                    fill(255);
+                    text(months[i], x + (w * 0.5f), height - (border * 0.5f));
+                }
+                text("Rainfall barchart", width * 0.5f, border * 0.5f);
                 break;
             }
             case 1: {
-                // Trend line
+                float border = width * 0.1f;
+                stroke(255);
+                colorMode(HSB);
+                line(border, border, border, height - border);
+                line(border, height - border, width - border, height - border);
+                textAlign(CENTER, CENTER);
+                for(float f = 0 ; f <= 120 ; f += 10)
+                {
+                    float y = map(f, 0, 120, height - border, border);
+                    line(border - 5, y, border, y);
+                    fill(255);
+                    text((int) f, border * 0.5f, y);
+                }
+
+                float w = (width - border * 2) / (float) rainfall.length;
+                for(int i = 0 ; i < rainfall.length ; i ++)
+                {
+                    float x1 = map(i , 0, rainfall.length, border, width - border);                    
+                    fill(255);
+                    text(months[i], x1 + (w * 0.5f), height - border * 0.5f);
+                }
+                for(int i = 1 ; i < rainfall.length ; i ++)
+                {
+                    float x1 = map(i - 1 , 0, rainfall.length - 1, border + (w * 0.5f), width - border - (w * 0.5f));
+                    float y1 = map(rainfall[i - 1], 0, 120, height - border, border);
+                    float x2 = map(i , 0, rainfall.length - 1, border + (w * 0.5f), width - border - (w * 0.5f));
+                    float y2 = map(rainfall[i], 0, 120, height - border, border);
+                    line(x1, y1, x2, y2);                    
+                }
             }
             case 2: {
                 // Pie chart
