2bc16aa39abe68a56768271a506680ba6a1fd9de
Bryan Duggan <skooter500@gmail.com>
2021-03-01 13:20:58 +0000

    wip

--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -1,8 +1,25 @@
 package ie.tudublin;
 
+import java.util.ArrayList;
+
 import processing.core.PApplet;
+import processing.data.Table;
+import processing.data.TableRow;
 
 public class StarMap extends PApplet {
+
+    ArrayList<Star> stars = new ArrayList<Star>();
+
+    void loadStars()
+    {
+        Table table = loadTable("HabHYG15ly.csv", "header");
+        for(TableRow row:table.rows())
+        {
+            Star s = new Star(row);
+            stars.add(s);
+        }
+    }
+
     public void settings() {
         size(500, 500);
     }
@@ -14,6 +31,7 @@ public class StarMap extends PApplet {
 
     public void setup() {
         colorMode(RGB);
+        loadStars();
     }
 
     public void draw() {
