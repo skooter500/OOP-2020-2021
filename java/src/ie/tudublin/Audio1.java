1d929a5fde2c32c05586db81341eeb20de57dd28
Bryan Duggan <skooter500@gmail.com>
2021-03-08 13:57:24 +0000

    bla

--- java/src/ie/tudublin/Audio1.java
+++ java/src/ie/tudublin/Audio1.java
@@ -13,10 +13,15 @@ public class Audio1 extends PApplet {
     AudioPlayer ap;
     AudioBuffer ab; // Samples
 
+    float[] lerpedBuffer;
+
     public void settings()
     {
         size(512, 512);
     }
+
+    float y = 200;
+    float lerpedY = y;
 
     public void setup()
     {
@@ -27,8 +32,12 @@ public class Audio1 extends PApplet {
         ab = ai.mix; // Connect the buffer to mic
         //ab = ap.mix; // Connect the buffer to the mp3 file
         colorMode(HSB);
+        lerpedBuffer = new float[width];
 
     }
+
+
+    float lerpedAverage = 0;
 
     public void draw()
     {
@@ -36,16 +45,27 @@ public class Audio1 extends PApplet {
         stroke(255);
         float halfHeight = height / 2;
         float average = 0;
+        float sum = 0;
         for(int i = 0 ; i < ab.size(); i ++)
         {
             float c = map(i, 0, ab.size(), 0, 255);
             stroke(c, 255, 255);
-            line(i, halfHeight - ab.get(i) * halfHeight, i, halfHeight + ab.get(i) * halfHeight);
+            lerpedBuffer[i] = lerp(lerpedBuffer[i], ab.get(i), 0.1f);
+
+            line(i, halfHeight - lerpedBuffer[i] * halfHeight * 4, halfHeight + lerpedBuffer[i] * halfHeight * 4, i);
             //println(ab.get(i));
+            sum += abs(ab.get(i));
         }
-
+        average = sum / (float) ab.size();
         // Calculate the AVERAGE amplitude
+        lerpedAverage = lerp(lerpedAverage, average, 0.1f);
+        ellipse(width / 4, 100, average * 500, average * 500);
+        ellipse(width / 2, 100, 50 + (lerpedAverage * 500), 50 + (lerpedAverage * 500));
 
-        ellipse(width / 2, 100, average * 100, average * 100);
+        
+        ellipse(200, y, 30, 30);
+        ellipse(300, lerpedY, 30, 30);
+        y += random(-10, 10);
+        lerpedY = lerp(lerpedY, y, 0.1f);        
     }   
 }
